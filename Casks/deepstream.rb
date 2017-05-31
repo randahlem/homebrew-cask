860f82c00473b7641c39d5c65a766d8a55639c82
randahlem <daniel.steig@deepstreamhub.com>
2017-05-31 16:59:12 +0200

    Add deepstream.pkg 2.3.0

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -5,7 +5,7 @@ cask 'deepstream' do
   url 'https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-#{version}.pkg'
   name 'Deepstream.io'
   homepage 'https://deepstream.io'
-
+  caveats 'Run mkdir -p /Library/Logs/deepstream before starting deepstream'
   pkg 'deepstream.io-#{version}.pkg'
   
   uninstall pkgutil: 'deepstream.io'
